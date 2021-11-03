ef86cbec555ed9952b47e8e7b23a673fb1f37e24
Robin Hesse <robin_hesse@gmx.net>
2021-11-03 12:29:55 +0100

    updated visualization

--- imagenet/utils/__init__.py
+++ imagenet/utils/__init__.py
@@ -1,3 +1,3 @@
 from .expected_gradients import AttributionPriorExplainer
 from .fixup import mixup_data, mixup_criterion
-from .visualization import Visualize, show_pil_image, pil_image
+from .visualization import show_pil_image, pil_img, visualize_attrs
